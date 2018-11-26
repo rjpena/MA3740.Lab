2c8dc0b55a9d23fe8119f7474bec89b5b05416aa
Ruben <43040757+rjpena@users.noreply.github.com>
2018-11-26 10:44:43 -0500

    Add files via upload

--- Project2/Project2.R
+++ Project2/Project2.R
@@ -2,36 +2,35 @@
 data<-read.csv('/home/campus21/rjpena/Desktop/MA3740/Project2/egg.csv',header=T)
 cor(data)
 
-#all predictors
+#Part A
 model<-lm((data$egg~data$age+data$weight+data$water+data$feed+data$time))
 #shows weight,water,feed as significant
-#88.56 multiple r^2, 2.024rse
-summary(model)
 
+summary(model)#88.56 multiple r^2, 2.024rse
+
+
+#Part B
 #-age,time
 model2<-lm((data$egg~data$weight+data$water+data$feed))
-#88.19 mr2, 1.993 rse
-summary(model2)
+summary(model2)#88.19 mr2, 1.993 rse
 
 #-age
 model3<-lm((data$egg~data$weight+data$water+data$feed+data$time))
-#88.47 mr2, 2.008rse
-summary(model3)
+summary(model3)#88.47 mr2, 2.008rse
 
 #-time
 model4<-lm((data$egg~data$age+data$weight+data$water+data$feed))
-#88.29mr2, 2.023rse
-summary(model4)
+summary(model4)#88.29mr2, 2.023rse
 
+
+#PartC
 x1<-70
 x2<-1800
 x3<-0.80
 x4<-110
 x5<-15
-
 y_hat<-(0.012321*x1+.023436*x2+14.630295*x3+0.139536*x4+0.171311*x5-14.006517)
-#58.663614
-y_hat
+y_hat#58.663614
 
 
 #Question 2
